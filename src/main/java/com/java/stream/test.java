fb50a786d5c3d1d99764e92e084cb96d20b4896e
leilimin <461083868@qq.com>
2021-11-10 15:22:10 +0800

    commit: test hello word

---
+++ src/main/java/com/java/stream/test.java
@@ -0,0 +1,12 @@
+package com.java.stream;
+
+/**
+ * @author LeiLiMin
+ * @Date 2021/11/10 15:21
+ * @Description whats stream
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
