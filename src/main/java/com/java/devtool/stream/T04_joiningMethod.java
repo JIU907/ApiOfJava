ff894ab2cd920ef190239470d634c85aab4c113a
leilimin <461083868@qq.com>
2021-11-12 18:02:32 +0800

    fix(stream): join

---
+++ src/main/java/com/java/devtool/stream/T04_joiningMethod.java
@@ -0,0 +1,23 @@
+package com.java.devtool.stream;
+
+import com.java.devtool.model.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author LeiLiMin
+ * @Date 2021/11/12 18:00
+ * @Description 测试拼接
+ */
+public class T04_joiningMethod {
+    public static void main(String[] args) {
+        List<User> users = User.getUsers();
+        String nameJoin = users.stream().map(User::getName).collect(Collectors.joining(","));
+
+        /**
+         * Lewis,Lucy,Mike,Lily
+         */
+        System.out.println(nameJoin);
+    }
+}
