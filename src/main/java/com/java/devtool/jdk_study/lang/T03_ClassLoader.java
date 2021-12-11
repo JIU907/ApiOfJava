f46f3e50d0e49367bc1eb3eadeeafcd3a5c22502
leilimin <461083868@qq.com>
2021-12-11 22:11:50 +0800

    docs: java.lang.ClassLoader
    1.add some docs for T03_ClassLoader

--- src/main/java/com/java/devtool/jdk_study/lang/T03_ClassLoader.java
+++ src/main/java/com/java/devtool/jdk_study/lang/T03_ClassLoader.java
@@ -6,4 +6,16 @@ package com.java.devtool.jdk_study.lang;
  * @date: 2021/12/11
  */
 public class T03_ClassLoader {
+    /**
+     * 1.ClassLoader负责对一个类进行加载
+     * 2.给定一个类的名称，一个classLoader应该去尝试确定或者生产一个(关于这个类的定义信息)数据
+     * 3.一个典型的策略就是根据文件系统中的一个名称去读取该"类文件"
+     *
+     * <p>每个Class对象包含一个ClassLoader去定义该Class(ClassLoader通过Class#getClassLoader()获取)</p>
+     *
+     * <p> 数组对象不通过class loader进行创建，但是必须自动的在java运行时被创建
+     *     调用数组对象的(Class#getClassLoader()),返回的是其元素的ClassLoader
+     *     如果其元素是原始的数据类型(primitive type)，这个数组的Class对象是没有ClassLoader的
+     *  </p>
+     */
 }
