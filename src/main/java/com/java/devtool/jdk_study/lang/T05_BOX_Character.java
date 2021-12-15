641dc148eddce7e6a898db0fb7c0eb0d2be4a45f
leilimin <461083868@qq.com>
2021-12-15 17:59:50 +0800

    feat(dev-notice): int Character

--- src/main/java/com/java/devtool/jdk_study/lang/T05_BOX_Character.java
+++ src/main/java/com/java/devtool/jdk_study/lang/T05_BOX_Character.java
@@ -6,4 +6,14 @@ package com.java.devtool.jdk_study.lang;
  * @date: 2021/12/15
  */
 public class T05_BOX_Character {
+    public static void main(String[] args) {
+        System.out.println((int)Character.reverseBytes((char)3));
+        /**
+         * (((ch & 0xFF00) >> 8) | (ch << 8))
+         * 字节反转：
+         *      eg: A B
+         *          B A
+         */
+        System.out.println(0xFF00);
+    }
 }
