b75460727de9d456e85ad684d38cb6fd50845279
leilimin <461083868@qq.com>
2021-12-20 18:11:19 +0800

    feat: RandomUtil
    1.通过已经使用的元素在全量数组上快速获取随机的可用元素

--- src/main/java/com/java/devtool/utils/randomutil/RandomUtil.java
+++ src/main/java/com/java/devtool/utils/randomutil/RandomUtil.java
@@ -4,8 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 /**
  * @Author LeiLiMin
@@ -71,5 +74,18 @@ public class RandomUtil {
             randomWeight -= weight.get(i);
         }
         return values.get(0);
+    }
+
+    /**
+     * 通过已经使用的元素在全量数组上快速获取随机的可用元素
+     * why?
+     * 1.主要是为了防止，当可用元素存量极少时，发送高概率的空转
+     */
+    public static <T> T test(T[] allData, List<T> used){
+        Set<T> currentIndex = used.stream().collect(Collectors.toSet());
+        List<T> usableIndex = Arrays.stream(allData).filter(e -> !currentIndex.contains(e)).collect(Collectors.toList());
+        int size = usableIndex.size();
+        int chooseIndex = ThreadLocalRandom.current().nextInt(size);
+        return usableIndex.get(chooseIndex);
     }
 }
