43ae8392a855e83c94d0daf949fb5949a326dfc1
leilimin <461083868@qq.com>
2021-12-30 18:48:21 +0800

    feat(Java.lang): String Builder
    1.introduce StringBuilder

--- src/main/java/com/java/devtool/jdk_study/lang/T07_StringBuilder.java
+++ src/main/java/com/java/devtool/jdk_study/lang/T07_StringBuilder.java
@@ -6,4 +6,11 @@ package com.java.devtool.jdk_study.lang;
  * @date: 2021/12/30
  */
 public class T07_StringBuilder {
+    /**
+     * StringBuilder是一个可变的字符序列
+     * 提供一些API兼容StringBuffer，但是是不保证同步的
+     * 设计该类的原因：在一些地方替换StringBuffer使用单一线程的问题
+     * 使用场景：并发问题不存在，或者效率优先的场景
+     * 主要操作：字符串中append,insert字符
+     */
 }
