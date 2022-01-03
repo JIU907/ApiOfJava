e118aa359fc09bb3d70025f5a1f5624622d635a8
leilimin <461083868@qq.com>
2022-01-03 18:09:53 +0800

    feat: java.lang.Byte
    1.cut-off some field

--- src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Byte.java
+++ src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Byte.java
@@ -6,4 +6,15 @@ package com.jiu907.api.jdk_source.lang.class_type.number;
  * @date: 2022/1/3
  */
 public class Box_Byte {
+    /**
+     * {@link Byte}:是基础数据类型byte的包装类型
+     * 它定义了2个常量
+     *      {@link Byte#value}: Byte变量的值
+     *      {@link Byte#MIN_VALUE}: -128
+     *      {@link Byte#MAX_VALUE}:  127
+     *      {@link Byte.ByteCache}: 这个私有的内部类其实缓存了[-128,127]的Byte
+     */
+    public static void main(String[] args) {
+
+    }
 }
