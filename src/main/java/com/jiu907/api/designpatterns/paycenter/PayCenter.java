1a872a454b88564693b35424f180f9e13c3e9282
leilimin <461083868@qq.com>
2022-01-05 15:52:59 +0800

    update(pom): update spring dependency

--- src/main/java/com/jiu907/api/designpatterns/paycenter/PayCenter.java
+++ src/main/java/com/jiu907/api/designpatterns/paycenter/PayCenter.java
@@ -1,29 +1,25 @@
 package com.jiu907.api.designpatterns.paycenter;
 
+import com.jiu907.api.designpatterns.paycenter.paystrategy.AbstractPayModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Map;
+import java.util.ServiceLoader;
+
 /**
  * @Author LeiLiMin
- * @Description:
+ * @Description: 支付中心设计模式构思
  * @date: 2022/1/4
  */
-public abstract class PayCenter {
-    public void payProcessor(){
-        beforeProcessor();
-        doProcessor();
-        afterProcessor();
+@SpringBootApplication
+@ComponentScan(basePackages = "com.jiu907.api.designpatterns.paycenter.paystrategy")
+public class PayCenter {
+    public static void main(String[] args) {
+        SpringApplication.run(PayCenter.class, args);
     }
-    public void beforeProcessor(){
-
-    }
-
-    // 1.验单
-    // 2.下单
-    // 3.结单
-    public abstract void doProcessor();
-
-
-    public  void afterProcessor(){
-
-    }
-
-
 }
