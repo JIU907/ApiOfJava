8865efc88abe1a4362a62dfabaf9a77bded7d7bd
leilimin <461083868@qq.com>
2022-01-07 14:21:05 +0800

    doc: generateId
    1.添加注释

--- src/main/java/com/jiu907/api/utils/GenerateId.java
+++ src/main/java/com/jiu907/api/utils/GenerateId.java
@@ -8,52 +8,61 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * @Author LeiLiMin
  * @Description: ID生成
- *               不触发，且随机
+ * 不触发，且随机
  * @date: 2022/1/6
  */
 public class GenerateId {
-    public static int prefix=0;
-    public static int id=0;
-    public static final int base=10000;
-    public static void main(String[] args) {
-        /**
-         *
-         * 2.随机取出值
-         * 3.取出的值存入记忆集合
-         */
-        List<Integer> prefixList=new ArrayList<>();
-        List<Integer> idList=new ArrayList<>();
+    // 前缀号池
+    public static List<Integer> prefixList = new ArrayList<>();
+    // ID号池
+    public static List<Integer> idList = new ArrayList<>();
 
-        // 1.有序集合
+    public static void main(String[] args) {
+        // 1.生产有序集合
         for (int i = 0; i < 1000; i++) {
             prefixList.add(i);
             idList.add(i);
         }
-        HashSet<Integer> prefixSet=new HashSet<>();
-        HashSet<Integer> idSet=new HashSet<>();
-        HashSet<Integer> resultSet=new HashSet<>();
 
-        while(prefixList.size()>0){
+        // 2.准备结果集
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        /**
+         * 生产ID主逻辑
+         *  1.从前缀号池中选出一个前缀，并将其从号池中移除
+         *  2.从ID号池中选取ID，并将其从ID号池中移除
+         *  3.直到ID号池中的ID被用完，再回到步骤1
+         */
+        while (prefixList.size() > 0) {
             int prefix = ThreadLocalRandom.current().nextInt(prefixList.size());
             Integer prefixRemove = prefixList.remove(prefix);
-            while(idList.size()>0){
+            while (idList.size() > 0) {
                 int id = ThreadLocalRandom.current().nextInt(idList.size());
                 Integer idRemove = idList.remove(id);
-                prefixSet.add(prefixRemove);
-                idSet.add(idRemove);
-                int i=prefixRemove *1000+idRemove;
+                int i = prefixRemove * 1000 + idRemove;
                 resultSet.add(i);
             }
             for (int i = 0; i < 1000; i++) {
                 idList.add(i);
             }
         }
-        System.out.println("resultSet"+resultSet.size());
-        System.out.println("prefixSet"+prefixSet.size());
-        System.out.println("idSet"+idSet.size());
+        System.out.println("resultSet" + resultSet.size());
 
-        // 前缀基数：10000
-        // 前缀生成：xxxxx
-        System.out.println(100000|99);
+    }
+
+    public static void redisImplements() {
+        /**
+         * redis实现细节
+         * local prefix=redis.call('srandmember',KEYS[1],ARGV[1]);
+         * if(type(prefix) == 'number') then
+         *   return prefix;
+         * end;
+         * return -1
+         *
+         *
+         *
+         *
+         */
+//  * local prefix=redis.call('srandmember',KEYS[1],ARGV[1]);if(type(prefix) == 'number') then return prefix;end;return -1
     }
 }
