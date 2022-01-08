0ee5a7d48bbde381aa2d9294c5a4b0990c74e275
leilimin <461083868@qq.com>
2022-01-08 19:40:18 +0800

    feat: java.lang.Byte
    1.ValueOf Method

--- src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Byte.java
+++ src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Byte.java
@@ -15,10 +15,24 @@ public class Box_Byte {
      *      {@link Byte.ByteCache}: 这个私有的内部类其实缓存了[-128,127]的Byte
      */
     public static void main(String[] args) {
-
+        byte b =1;
+        testValueOfMethod(b);
     }
 
     public static void testValueOfMethod(byte b){
+        /**
+         * 可以理解为将一个基础数据类型的byte转为包装类型的Byte
+         */
+        System.out.println(Byte.valueOf(b));
 
+        /**
+         * 字符串表示的byte转为包装类型的Byte
+         */
+        System.out.println(Byte.valueOf("-1"));
+
+        /**
+         * 将16进制表示的"A"转为10进制，以Byte的形式来存储
+         */
+        System.out.println(Byte.valueOf("A", 16));
     }
 }
