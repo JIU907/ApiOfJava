34e299772017a72051d11269397c966fd9783384
leilimin <461083868@qq.com>
2022-01-10 22:36:58 +0800

    feat: GenerateId.lua v1

--- src/main/java/com/jiu907/api/utils/generateId/GenerateId.lua
+++ src/main/java/com/jiu907/api/utils/generateId/GenerateId.lua
@@ -2,39 +2,53 @@
 -- Lua脚本__生成UserId --
 -------------------------
 
-
 -- 因为redis不允许执行一些不确定的命令，所以引入此操作
 redis.replicate_commands();
 
-
 -- 前缀池大小
-local prefixPoolSize = 10000;
+local prefixPoolSize = 5;
 -- ID池大小
-local idPoolSize     = 10000;
+local idPoolSize     = 5;
 -- 当前前缀Key
-local currentPrefixName='currentPrefix';
+local currentPrefix_key='currentPrefix';
 -- 当前前缀池
-local prefixPoolName='prefixPool';
+local prefixPool_key='prefixPool';
 -- 当前Id池
-local idPoolName='idPool';
+local idPool_key='idPool';
 -- 当前前缀
 local currentPrefixValue=redis.call("get",KEYS[1]);
 
 -- 1.初始化前缀和ID号池，利用Redis的murmurhash2算法无序生成
 --   初始化currentPrefix
 if(type(currentPrefixValue)=='boolean') then
-    redis.call('del',prefixPoolName,idPoolName);
+    redis.call('del',prefixPool_key,idPool_key);
     for i=1,prefixPoolSize do
-         redis.call('sadd',prefixPoolName,i);
-         redis.call('sadd',idPoolName,i);
+        redis.call('sadd',prefixPool_key,i);
+        redis.call('sadd',idPool_key,i);
     end
-    currentPrefixValue = redis.call('spop',prefixPoolName);
-    redis.call('set',currentPrefixName,currentPrefixValue);
+    currentPrefixValue = redis.call('spop',prefixPool_key);
+    redis.call('set',currentPrefix_key,currentPrefixValue);
 end
 
--- 2.取号操作
+-- 2.获取Id
+local idNumber = redis.call('spop',idPool_key);
+--   IdPool.size()<=0 && prefixPool.size<=0 => processor is over!
+--   IdPool.size()<=0 && prefixPool.size>0  => produce idPool
+if(type(idNumber)=='boolean') then
+    currentPrefixValue = redis.call('spop',prefixPool_key);
+    if( type(currentPrefixValue)=='boolean') then
+        return nil;
+    end;
+    redis.call('set',currentPrefix_key,currentPrefixValue);
+    for i=1,prefixPoolSize do
+        redis.call('sadd',idPool_key,i);
+    end
+    idNumber = redis.call('spop',idPool_key);
+end
+redis.call('set','currentId',idNumber);
 
+-- 3.生成最终User_Id => [100 000 , 1 000 000 000]
+currentPrefixValue=tonumber(currentPrefixValue);
+idNumber=tonumber(idNumber);
 
-
-
-return currentPrefixValue;
+return currentPrefixValue*10000+idNumber;
