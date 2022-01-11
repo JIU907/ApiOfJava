184047c510d0dfd628101baf9331669d4ac3775a
leilimin <461083868@qq.com>
2022-01-11 17:27:51 +0800

    feat(java.lang): Integer
    1.about Integer's field

--- src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Integer.java
+++ src/main/java/com/jiu907/api/jdk_source/lang/class_type/number/Box_Integer.java
@@ -6,4 +6,12 @@ package com.jiu907.api.jdk_source.lang.class_type.number;
  * @date: 2022/1/11
  */
 public class Box_Integer {
+    /**
+     * 属性值
+     * {@link Integer#MIN_VALUE}=0x80000000= 10000000 00000000 00000000 00000000
+     * {@link Integer#MIN_VALUE}=0x7fffffff= 01111111 11111111 11111111 11111111
+     * {@link Integer#value}    = int Value
+     * {@link Integer#SIZE}     = Integer代表的是一个32位的int
+     * {@link Integer#BYTES}    = Integer代表的是一个4字节的int
+     */
 }
