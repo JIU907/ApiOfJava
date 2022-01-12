9f70fcbb1d8311f728fd369e842c34fb84ab478d
leilimin <461083868@qq.com>
2022-01-12 22:11:09 +0800

    feat(payCenter): ApplePay
    1.init strategy

---
+++ src/main/java/com/jiu907/api/designpatterns/paycenter/paystrategy/ApplePay.java
@@ -0,0 +1,13 @@
+package com.jiu907.api.designpatterns.paycenter.paystrategy;
+
+/**
+ * @Author LeiLiMin
+ * @Description: 苹果支付
+ * @date: 2022/1/12
+ */
+public class ApplePay extends AbstractPayModel {
+    @Override
+    protected void doProcessor() {
+
+    }
+}
