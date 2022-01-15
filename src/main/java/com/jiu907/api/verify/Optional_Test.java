49e919eb84d96f07a664f0a5ac5a23ae524a278f
leilimin <461083868@qq.com>
2022-01-15 18:53:53 +0800

    feat(verify): Optional

---
+++ src/main/java/com/jiu907/api/verify/Optional_Test.java
@@ -0,0 +1,28 @@
+package com.jiu907.api.verify;
+
+import java.util.Optional;
+
+/**
+ * @Author LeiLiMin
+ * @Description:
+ * @date: 2022/1/14
+ */
+public class Optional_Test {
+    public static void main(String[] args) {
+        String str = null;
+        // 允许arg为null
+        Optional<String> str2 = Optional.ofNullable(str);
+
+        // 返回： value!=null
+        System.out.println(str2.isPresent());
+
+        // return: value
+        String s = str2.get();
+
+        // 如果arg=null,会抛出异常
+        Optional<String> str1 = Optional.of(str);
+
+        //  value != null ? value : other;
+        String s1 = str1.orElse("1");
+    }
+}
