bcce563dd7847c40400659635eb76459f6bf4614
leilimin <461083868@qq.com>
2022-01-27 19:06:54 +0800

    fix: Spring
    1.@Profile
    2.@Scope

--- src/main/java/com/jiu907/api/designpatterns/paycenter/controller/PayController.java
+++ src/main/java/com/jiu907/api/designpatterns/paycenter/controller/PayController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.http.HttpRequest;
 import java.util.Map;
 
 /**
