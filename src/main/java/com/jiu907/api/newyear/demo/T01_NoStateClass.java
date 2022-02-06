60d7f8a8b15de7a436f33840df164fb2a18de92f
leilimin <461083868@qq.com>
2022-02-06 17:03:20 +0800

    feat: Java并发编程实战-第二章
    1.非原子操作下的并发问题

--- src/main/java/com/jiu907/api/newyear/demo/T01_NoStateClass.java
+++ src/main/java/com/jiu907/api/newyear/demo/T01_NoStateClass.java
@@ -10,14 +10,28 @@ import java.util.concurrent.ThreadLocalRandom;
 public class T01_NoStateClass {
     /**
      * 无状态对象--
-     *      不包含任何域
-     *      不包含其他类中域的引用
-     *      计算过程中的临时状态都保存在线程栈中的局部变量表
+     * 不包含任何域
+     * 不包含其他类中域的引用
+     * 计算过程中的临时状态都保存在线程栈中的局部变量表
+     *
      * @param bound
      * @return
      */
-    public static Integer getNum(Integer bound){
+    public static Integer getNum(Integer bound) {
         int random = ThreadLocalRandom.current().nextInt(bound);
         return random;
+    }
+
+    private int a = 0;
+
+    /**
+     * 并非线程安全--
+     * a++: 并发原子操作
+     * 该对象可能被并发访问
+     * @return
+     */
+    public int increaseA() {
+        this.a++;
+        return this.a;
     }
 }
