ae3b509bee9482201a54b273398b71caa197b62b
leilimin <461083868@qq.com>
2022-02-06 17:26:17 +0800

    feat: Java并发编程实战-第二章
    1.内置锁--同步代码块

--- src/main/java/com/jiu907/api/newyear/demo/T03_UnsafeCache.java
+++ src/main/java/com/jiu907/api/newyear/demo/T03_UnsafeCache.java
@@ -15,6 +15,7 @@ public class T03_UnsafeCache {
      * 线程不安全--
      * 这个方法对于longNumber和longString的存值操作并不能保证是原子的操作
      * 也就是不能保证longNumber和longString取值时保持对应关系
+     *
      * @param num
      */
     public void processor(Long num) {
@@ -23,6 +24,22 @@ public class T03_UnsafeCache {
         } else {
             longNumber.set(num);
             longString.set(num + ":String");
+        }
+    }
+
+    /**
+     * 通过内置锁--
+     * 同步代码块来保证代码段的原子操作
+     * @param num
+     */
+    public void processorSync(Long num) {
+        synchronized (this) {
+            if (num.equals(longNumber.get())) {
+                System.out.println(longString.get());
+            } else {
+                longNumber.set(num);
+                longString.set(num + ":String");
+            }
         }
     }
 }
