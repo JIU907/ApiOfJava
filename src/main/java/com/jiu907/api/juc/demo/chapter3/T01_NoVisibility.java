a0abc2353efc268faa1a5f4714ecf36776094035
leilimin <461083868@qq.com>
2022-02-07 22:16:02 +0800

    feat(juc): 第三章
    1.可见性

--- src/main/java/com/jiu907/api/juc/demo/chapter3/T01_NoVisibility.java
+++ src/main/java/com/jiu907/api/juc/demo/chapter3/T01_NoVisibility.java
@@ -26,4 +26,44 @@ public class T01_NoVisibility {
         }, "Thread-write").start();
 
     }
+
+
+    /**
+     * 以下 get/set方法再遇到并发时，可能导致get的值未必时最新set的值
+     */
+    private int value;
+
+    private int getValue() {
+        return value;
+    }
+
+    private void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * 非volatile修饰的共享变量
+     *
+     * JMM要求--
+     * 变量的读取操作和写入操作都必须时原子操作
+     * 但是对于long和double这种64位变量，JVM允许将读写操作分解为2个32位操作
+     *  这就导致了程序很有可能操作的是一个随机数，而不是我们曾经设置的一个数
+     *
+     */
+
+
+    /**
+     * 一种弱同步的方式--
+     * 用来保证变量的可见性
+     */
+    private volatile int valueVisibility;
+
+    private int getValueVisibility() {
+        return valueVisibility;
+    }
+
+    private void setValueVisibility(int value) {
+        this.valueVisibility = valueVisibility;
+    }
+
 }
