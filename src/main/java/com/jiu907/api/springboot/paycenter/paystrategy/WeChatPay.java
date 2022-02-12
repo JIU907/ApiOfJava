e81902143dba7428d5144ae4b77556a1a228af81
leilimin <461083868@qq.com>
2022-02-12 19:53:36 +0800

    feat(springboot): profile
    1.后续找一个合适的案例

--- src/main/java/com/jiu907/api/springboot/paycenter/paystrategy/WeChatPay.java
+++ src/main/java/com/jiu907/api/springboot/paycenter/paystrategy/WeChatPay.java
@@ -1,5 +1,6 @@
 package com.jiu907.api.springboot.paycenter.paystrategy;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,6 +9,12 @@ import org.springframework.stereotype.Component;
  * @date: 2022/1/5
  */
 @Component
+/**
+ * 环境变量添加该参数：如果@Profile中的value等于以下参数则该Bean会被注入
+ *                  反之则不会被注入
+ * --spring.profiles.active=prd
+ */
+@Profile(value = "dev")
 public class WeChatPay extends AbstractPayModel{
     @Override
     public void doProcessor(String payload) {
