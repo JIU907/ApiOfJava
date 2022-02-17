67c92de7812cfde36bd388dff6e5de7eb65fa9eb
leilimin <461083868@qq.com>
2022-02-17 18:26:43 +0800

    feat(jdk8): localDateTime
    1.获取当日最小时间和最大时间的毫秒值

--- src/main/java/com/jiu907/api/jdk8/T01_LocalDateTime.java
+++ src/main/java/com/jiu907/api/jdk8/T01_LocalDateTime.java
@@ -1,6 +1,8 @@
 package com.jiu907.api.jdk8;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneOffset;
 
 /**
@@ -14,6 +16,13 @@ public class T01_LocalDateTime {
         Long second = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
         // 2.获取毫秒数
         Long milliSecond = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+
+        // 3.获取当前时区-当日最小时间 00:00:00 的毫秒值
+        long min = LocalDateTime.of(LocalDate.now(), LocalTime.MIN).toInstant(ZoneOffset.of("+8")).toEpochMilli();
+
+        // 4.获取当前时区-当日最小时间 23:59:59 的毫秒值
+        long max = LocalDateTime.of(LocalDate.now(), LocalTime.MAX).toInstant(ZoneOffset.of("+8")).toEpochMilli();
+
         System.out.println(milliSecond.equals(System.currentTimeMillis())); //true
     }
 }
