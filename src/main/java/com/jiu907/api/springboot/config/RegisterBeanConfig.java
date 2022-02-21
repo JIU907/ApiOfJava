638ccc2bf40a198cc2258dd28b736710034ceec8
leilimin <461083868@qq.com>
2022-02-21 20:06:16 +0800

    fix: spring boot
    1.RegisterBeanConfig的@value注解就行修复
      将#->$

--- src/main/java/com/jiu907/api/springboot/config/RegisterBeanConfig.java
+++ src/main/java/com/jiu907/api/springboot/config/RegisterBeanConfig.java
@@ -3,7 +3,9 @@ package com.jiu907.api.springboot.config;
 import com.jiu907.api.verify.sensorsbi.SensorService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author LeiLiMin
@@ -16,7 +18,7 @@ public class RegisterBeanConfig {
      * 以@Bean的形式将神策服务加入到Spring上下文中
      */
     @Bean
-    public SensorService configuration(@Value("#{spring.application.name}")String fileName){
+    public SensorService configuration(@Value("${spring.application.name}")String fileName){
         return new SensorService(fileName);
     }
 }
