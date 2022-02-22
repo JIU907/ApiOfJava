19de927ba6a9fc44c73173489b78ba77e90714d0
leilimin <461083868@qq.com>
2022-02-22 23:21:42 +0800

    feat(juc): 组合方式为线程安全类添加功能

---
+++ src/main/java/com/jiu907/api/juc/demo/chapter4/T08_ImprovedList.java
@@ -0,0 +1,162 @@
+package com.jiu907.api.juc.demo.chapter4;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * @Author LeiLiMin
+ * @Description: 组合的方式进行加锁
+ * @date: 2022/2/22
+ */
+public class T08_ImprovedList<E> implements List<E> {
+    private final List<E> list;
+
+    public T08_ImprovedList(List<E> list) {
+        this.list = list;
+    }
+
+    /**
+     * 怎么实现线程安全的呢？
+     * 1.对于List的操作委托给底层的List实例来操作
+     * 2.访问List对象都调用同步方法来操作
+     * 3.并且假设客户端并不会操作list的元素
+     * -> 如果要操作，要加其他的措施
+     * 4.当显然通过组合的方式要由于其他方式，对于目标对象的操作都在一个类中进行操作要更合适
+     *
+     * @param x
+     * @return
+     */
+    public synchronized boolean putIfAbsent(E x) {
+        boolean absent = !list.contains(x);
+        if (absent) {
+            list.add(x);
+        }
+        return absent;
+    }
+
+    // ==============不要看=================
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @NotNull
+    @Override
+    public <T> T[] toArray(@NotNull T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(@NotNull Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(@NotNull Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, @NotNull Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(@NotNull Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(@NotNull Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
