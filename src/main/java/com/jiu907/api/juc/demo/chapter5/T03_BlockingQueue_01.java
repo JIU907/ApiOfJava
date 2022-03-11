71062237b3693d0a14317f0a204580afad9f7a38
leilimin <461083868@qq.com>
2022-03-11 22:27:47 +0800

    feat(juc): 利用阻塞队列设计一个简单的生产者消费者模式

---
+++ src/main/java/com/jiu907/api/juc/demo/chapter5/T03_BlockingQueue_01.java
@@ -0,0 +1,66 @@
+package com.jiu907.api.juc.demo.chapter5;
+
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @Author LeiLiMin
+ * @Description: 阻塞队列实现生产者消费者模式 01
+ * @date: 2022/3/11
+ * <p>
+ * 模拟了一个
+ * 1.可支持自由输入数据
+ * 2.消费者不断处理数据的建议，消费者-生产者模型
+ */
+public class T03_BlockingQueue_01 {
+    private final Producer producer;
+    private final Consumer consumer;
+
+
+    public T03_BlockingQueue_01() {
+        ArrayBlockingQueue<String> taskQueue = new ArrayBlockingQueue(100);
+        producer = new Producer(taskQueue);
+        consumer = new Consumer(taskQueue);
+    }
+
+    public void producerData(String string) throws InterruptedException {
+        producer.produceData(string);
+    }
+
+    // 生产者
+    class Producer {
+        private final ArrayBlockingQueue<String> taskQueue;
+
+        public Producer(ArrayBlockingQueue<String> taskQueue) {
+            this.taskQueue = taskQueue;
+        }
+
+        public void produceData(String data) throws InterruptedException {
+            taskQueue.put(data);
+        }
+    }
+
+    // 消费者
+    class Consumer implements Runnable {
+        private final ArrayBlockingQueue<String> taskQueue;
+
+        public Consumer(ArrayBlockingQueue<String> taskQueue) {
+            this.taskQueue = taskQueue;
+        }
+
+        public String consumeData() throws InterruptedException {
+            return taskQueue.take();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    consumeData();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
